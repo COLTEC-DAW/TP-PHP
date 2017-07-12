e00f7872efe2ad76d8f7b887d446ce905dc502be
VictorHSMoura <victorhugosmoura@gmail.com>
2017-07-12 18:54:07 -0300

    Atualizando a página das mesas!

--- pgMesa.php
+++ pgMesa.php
@@ -13,6 +13,10 @@ $mesa = pegaPorId($todasAsMesas, $idMesa); ?>
     <head>
         <title><?=$mesa->nome?></title>
         <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="STYLE/style.css"></link>
     </head>
     <body> <?php
         if (entrada) {
@@ -28,17 +32,23 @@ $mesa = pegaPorId($todasAsMesas, $idMesa); ?>
             ?> <h2>Esta é uma mesa privada.</h2> <?php
         }
         else { ?>
-            <h1><?=$mesa->nome?></h1>
-            <p><strong>Mestre: </strong><?= $mesa->mestre ?></p>
-            <p><strong>Sistema: </strong><?= $mesa->sistema ?></p>
-            <p><strong>Gênero: </strong><?= $mesa->genero ?></p>
-            <p><?= $mesa->sinopse ?></p>
-            <p><strong>Endereço: </strong><?= $mesa->endereco ?></p>
+            <div class="container-fluid">
+                <? require "INC/userSideBar.inc"; ?>
+                <div class="col-sm-10 centerbar">
+                    <h1><?=$mesa->nome?></h1>
+                    <div class="divider"></div>
+                    <p><strong>Mestre: </strong><?= $mesa->mestre ?></p>
+                    <p><strong>Sistema: </strong><?= $mesa->sistema ?></p>
+                    <p><strong>Gênero: </strong><?= $mesa->genero ?></p>
+                    <p><?= $mesa->sinopse ?></p>
+                    <p><strong>Endereço: </strong><?= $mesa->endereco ?></p>
+                </div>
             <?php   /* Comentando por não estar sendo usado agora
                     *listaJogadores();
                     */
         }
+        include "INC/footer.inc";
 ?>
+            </div>
 </body>
 </html>
-<?php include "INC/footer.inc"; ?>
