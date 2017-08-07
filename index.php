7c2a92c6592165ef26230275d0a52f5ba116f759
franciscofabneves <franciscofabneves@gmail.com>
2017-08-07 17:18:30 -0300

    Tem que integrar login VERSAO INCOMPLETA

--- index.php
+++ index.php
@@ -1,5 +1,60 @@
-<!DOCTYPE>
-<!-- Página inicial, apresentada antes do usuário logar-->
+<?php
+// Página inicial, apresentada antes do usuário logar
+session_start();
+require "INC/funcoes.inc";
+
+function validar($login, $senha){
+    $arquivo = fopen("users.json", 'r');
+    $txt = "";
+    while(!feof($arquivo)) $txt .= fgets($arquivo);
+    $json = json_decode($txt);
+    fclose($arquivo);
+    foreach ($json as $pessoa) {
+        if ($pessoa->login == $login && $pessoa->senha == $senha) return true;
+    }
+    return false;
+}
+
+        // Verifica se login e a senha vem da sessao ou do post
+		if(!$_SESSION["login"]) {
+			$login = $_POST["login"];
+			$senha = $_POST["senha"];
+		}
+        else {
+			$login = $_SESSION["login"];
+			$senha = $_SESSION["senha"];
+		}
+		if(!$login) { //Procedimento se não estiver logado
+			header("Location: login.php");
+		}
+        else if(validar($login, $senha)) {//Senha correta
+            echo "<h2>Login bem sucedido</h2>";
+			$_SESSION["login"] = $login;
+			$_SESSION["senha"] = $senha;
+            ?>
+            <!Doctype>
+            <html>
+                <head>
+                    <title>Bem vindo</title>
+                </head>
+                <body>
+                    <h1>BEM VINDO AO SHOW DO BILHÃO</h1>
+                    <p>As regras são claras! Responda às perguntas SEM ERRAR e ganhe UM BILHÃO</p>
+                    <form method="get" action="perguntas.php">
+                        <input type="hidden" name="num" value="0">
+                        <button type="submit">Começar</button>
+                    </form>
+            <?php
+        }
+        else { //Senha incorreta
+			?>
+            <h2>Falha no login</h2>
+			<a href='/login.php'>Tentar Novamente</a>
+            <?php
+		}
+        include "rodape.inc"; ?>
+    </body>
+</html>
 <html>
     <head>
         <title>GameMaster</title>
@@ -10,6 +65,8 @@
         <meta charset=utf-8>
     </head>
     <body>
-        <a href="home.php">Go Home!</a>
+        <a href="home.php">Go Home!</a> <br>
+        <a href="">Novo usuário</a> <br>
+        <a href="">Logar</a>
     </body>
 </html>
