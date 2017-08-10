c6f2e3405352fdd90ecb6036b1dc35a3f7059d40
Clarisse Scofield <clarisse.scofield@gmail.com>
2017-08-10 11:03:28 -0300

    Mudanças
    Inicio responsividade.
    Página cadastro com css.
    Página admin.

--- tentativabanco/adminlogin.php
+++ tentativabanco/adminlogin.php
@@ -7,8 +7,7 @@
 	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script src="jquery.flip.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link href="//fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" type="text/css"/>
 	<script src="js/cover.js"></script> <!-- cover -->
 	<script>    AOS.init();  </script>
@@ -20,62 +19,13 @@
 
 
 <body>
-	<!-- ********** COVER VIDEO ************* !-->
-
-		
-
-
-	<div class="container">
-
-		<div class="homepage-hero-module">
-		<div class="visible-lg video-container">
-			<div class=title-container>
-					<img src="img/covericon.png" alt="">
-					<br>
-					<button type="button" class="btn btn-lg" id="start" align="center">start</button>
-					
-			</div>
-			<div class="filter"></div>
-			<video autoplay loop class="fillWidth">
-				<source src="cover/MP4/video" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
-				<source src="cover/WEBM/video.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
-			</video>
-			<div class="poster hidden">
-				<img src="cover/Snapshots/foto.jpg" alt="">
-			</div>
-		</div>
-		</div>
-
-	</div>
-
-	<!-- ********** COVER IMG ************* !-->
-	<div class="container">
-		
-			<div class="hidden-lg">
-				<div class=title-container>
-					<img src="img/covericon.png" alt="">
-					<br>
-					<button type="button" class="btn btn-lg" id="start" align="center">start</button>
-					
-				</div>
-			</div>
-		
-
-
-	</div>
-
-
 
 	<div id=topo>
 		<a name="ancora1" id="ancora1"></a>
 		 <nav id="menu">
 		    <ul>
 		    	<br><br>
-		        <li><a href="#">HOME</a></li>
-		        <li><a href="#" id="quemsomos">QUEM SOMOS</a></li>
-		        <li><a href="#">SOBRE</a></li>
-				<li><a href="#">CONTATO</a></li>
-				<li><a href="adminlogin.php">ADMIN</a></li>
+		        <li><a href="#">LOGIN ADMINISTRADOR</a></li>
 		    </ul>
 		</nav>
 		<h1 id="title"> CVIU </h1>
@@ -86,7 +36,7 @@
 	<div class="row">
 		<div class="col-md-4"></div>
 		<div class="col-md-4">
-			<h2 class="card-title">login</h2>
+			<h2 class="card-title">login admin</h2>
 						<form action="login.php" method="post">
 							<div class="input-group">
 								<label for="email">Email </label>
@@ -98,7 +48,7 @@
 							</div>
 							<br>
 							 <input type="submit" id="button" class="sb bradius" value="Entrar" />
-							 <a href="cadastro.php"> <input type="button" id="buttton" class="sb bradius" value="Cadastre-se" /></a>
+							 
 							 
 						</form>
 						
@@ -106,23 +56,7 @@
 		<div class="col-md-4"></div>
 	</div>
 
-	<div id=sobre>
-			<a name="ancora2" id="ancora2"></a>
-	        <h1 style="color: white;">Quem somos</h1>
-	        <h2 > Blablablabla blablabla blabalba</h2>
-			<div class="row">
-					<div class="col-md-4">
-						
-						<img class="img-circle" src="https://kaksimedia.com/kaxi/wp-content/uploads/2015/11/round.jpg">
-					</div>
-					<div class="col-md-4">
-						<img class="img-circle" src="https://kaksimedia.com/kaxi/wp-content/uploads/2015/11/round.jpg">
-					</div>
-					<div class="col-md-4">
-						<img class="img-circle" src="https://kaksimedia.com/kaxi/wp-content/uploads/2015/11/round.jpg">
-					</div>
-			</div>
-	</div>
+	
 	<br><br><br>
 	
 </body>
--- tentativabanco/cadastro.php
+++ tentativabanco/cadastro.php
@@ -3,28 +3,53 @@
 
 
     <head>
-        <title></title>
-    
+    <meta charset="UTF-8">
+	<meta name="robots" content="noindex, nofollow" />
+	<title>CVIU</title>
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="jquery.flip.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="css/cadastro.css" type="text/css" rel="stylesheet" />
 
        
 </head>
     
     <body>
+    <div id="header">
+	        <h1>Cadastro CVIU</h1>
+	</div>
 
-    <div id="cadastrar"><a style="text-decoration:none" href="index.php"> <input type="button" class="sb bradius" value="Login" /></a>
+
+
+    <div id="cadastrar">
+        <a style="text-decoration:none" href="index.php"> <input type="button" id="button" class="sb bradius" value="Login" /> </a>
 	<div id="login" class="form bradius" style="top:150px;">
-    	
-        
-        <div class="acomodar">
-        	<form action="cadastrando.php" method="post">
-            <label for="nome">Nome: </label><input id="nome" type="text" class="txt bradius" name="nome"  />
-            <label for="email">E-mail: </label><input id="email" type="text" class="txt bradius" name="email" />
-            <label for="senha">Senha: </label><input  id="senha" type="password" class="txt bradius" name="senha"  />
-            <input type="submit" class="sb bradius" value="Cadastrar" />
-            </form>
-        <!--acomodar-->
-        </div>
-       <!--login-->
-    </div>
+
+    <div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<h2 class="card-title">cadastro</h2>
+						<form action="cadastrando.php" method="post">
+							<div class="input-group">
+								<label for="nome">Nome </label>
+								<input id="nome" type="text" class="form-control" name="nome" placeholder="Nome">
+							</div>
+                            <div class="input-group">
+								<label for="email">Email </label>
+								<input id="email" type="text" class="form-control" name="email" placeholder="Email">
+							</div>
+							<div class="input-group">
+								<label for="senha">Senha </label>
+								<input id="senha" type="password" class="form-control" name="senha" placeholder="Senha">
+							</div>
+							<br>
+							 <input type="submit" id="button" class="sb bradius" value="Cadastrar" />
+						</form>
+						
+     		 	</div>
+		<div class="col-md-4"></div>
+	</div>
+
 </body>
 </html>
