dc90d10bfccfb74bad91bbe765389265f3deb341
VictorHSMoura <victorhugosmoura@gmail.com>
2017-08-10 10:45:28 -0300

    Alterando página de login

--- login.php
+++ login.php
@@ -9,14 +9,21 @@
         <meta charset=utf-8>
     </head>
     <body>
-        <h2 class= "fontebranca">Parece que você não está logado no GameMaster...</h2>
-        <form action="index.php" method="post">
-            Login: <input type="text" name="login">
-            Senha: <input type="password" name="senha">
-            <input type="submit" value="LogIn">
-        </form>
-        <h3 class= "fontebranca">Não tem conta?</h3>
-        <a href="cadastro.php">Crie uma aqui</a>
-        <?php include "INC/footer.inc" ?>
+        <div class="container-fluid">
+            <div class="col-sm-2 sidebar"></div>
+            <div class="col-sm-10 centerbar">
+                <h2 class= "fonteMarrom">Parece que você não está logado no GameMaster...</h2>
+                <form action="index.php" method="post">
+                    Login: <input type="text" name="login">
+                    Senha: <input type="password" name="senha">
+                    <input type="submit" value="LogIn">
+                </form>
+                <h3 class= "fonteMarrom">Não tem conta?</h3>
+                <a href="cadastro.php">Crie uma aqui</a>            
+            </div>
+        </div>
+        <div class="footer">
+            <?php include "INC/footer.inc" ?>
+        </div>
     </body>
 </html>
