ba603a0c387d500916e9601bd45c8346d5480e33
Lucas <lucaspaulom@hotmail.com>
2017-09-06 01:32:40 -0300

    Add files via upload

--- help.ME/email/manda_email.php
+++ help.ME/email/manda_email.php
@@ -1,6 +1,7 @@
 <?php
+	require $_SERVER['DOCUMENT_ROOT'] . '/email/phpmail/PHPMailerAutoload.php';
 
-	function sendEmail($email, $id_email_conf){
+	function sendEmail($email, $id_email_conf, $nome){
 		$mail = new PHPMailer;
 		$mail->isSMTP();
 		$mail->Host = 'smtp.gmail.com';
@@ -18,6 +19,7 @@
 		$link = 'https://help-me-daw.herokuapp.com/email/verifica_email.php?id_email='.$id_email_conf;
 		$mail->Subject = 'Valide sua conta';
 		$mail->Body    = "<h1>Teste</h1>
+						<h2>$nome</h2>
 						<a href='$link'>Verifique sua conta.</a>";
 
 		//$mail->Body    = file_get_contents(HTMLEMAILPATH);
