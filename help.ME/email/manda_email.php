33746eae5d996be0cac13003a19ce58361cf7681
Lucas <lucaspaulom@hotmail.com>
2017-09-06 20:11:44 -0300

    Add files via upload

--- help.ME/email/manda_email.php
+++ help.ME/email/manda_email.php
@@ -30,4 +30,34 @@
 		else 
 			return 'done';
 	}
+
+	function manda_doacao($email, $nome, $meta){
+		$mail = new PHPMailer;
+		$mail->isSMTP();
+		$mail->Host = 'smtp.gmail.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = 'lukvailox@gmail.com';
+		$mail->Password = '34960550.';
+		$mail->SMTPSecure = 'tls';
+		$mail->Port = 587;
+		$mail->CharSet = 'UTF-8';
+		
+		$mail->From = 'lukvailox@gmail.com';
+		$mail->FromName = 'Help.me';
+		$mail->addAddress($email,"Nome do cara");
+		$mail->isHTML(true);
+		$mail->Subject = 'Nova Doação';
+		$mail->Body    = "<h1>$nome</h1>
+						<h2>$meta</h2>
+						<p>Ajude em nosso site.</p>";
+
+		//$mail->Body    = file_get_contents(HTMLEMAILPATH);
+
+		$result = $mail->send(); 
+		if(!$result)
+			return 'Mailer Error: ' . $mail->ErrorInfo . "\n";
+		else 
+			return 'done';
+	}
+
 ?>
