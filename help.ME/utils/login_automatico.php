f4f611ea1452fc6b1bac81046d70e3c13dacba02
Lucas <lucaspaulom@hotmail.com>
2017-09-07 20:41:46 -0300

    Add files via upload

--- help.ME/utils/login_automatico.php
+++ help.ME/utils/login_automatico.php
@@ -1,31 +1,23 @@
 <?php
-
-    $arquivo = file_get_contents('../utils/usuario_logado.json');
-    $json = json_decode($arquivo);
-
-    $login;
-    $senha;
-
-    foreach ($json as $temp) {
-    	$login = $temp->login;
-    	$senha = $temp->senha;
-    }
-
-    if($login != null && $senha !=null){
-	?>
-	    <div class="card">
-
-		    <form action="../usuario/conf_login.php" method="post" enctype="multipart/form-data">
-
-		        <input type="hidden" name="nome" value=<?=$login?>>
-		        <input type="hidden" name="senha" value=<?=$senha?>>
-		        <p class="center-align">Continuar como <?=$login?> <?=$senha?> ?</p>
-		        <div class="center-align input-field">
-		            <input type="submit" name="Enviar" class="btn" value="Sim">
-		        </div>
-
-		    </form>
-		</div>
-		<?php
-	}
+    $login = "<script>document.writeln(localStorage.getItem('login'));</script>";
+    $senha = "<script>document.writeln(localStorage.getItem('senha'));</script>";
 ?>
+
+    <div class="card">
+
+	    <form action="../usuario/conf_login.php" method="post" enctype="multipart/form-data">
+
+	        <input id="login_automatico" type="hidden" name="nome" value="">
+	        <input id="senha_automatica" type="hidden" name="senha" value="">
+	        <p class="center-align">Continuar como <?=$login?>?</p>
+	        <div class="center-align input-field">
+	            <input type="submit" name="Enviar" class="btn" value="Sim">
+	        </div>
+
+	    </form>
+	</div>
+
+	<script type="text/javascript">
+		document.getElementById("login_automatico").setAttribute("value", localStorage.getItem('login'));
+		document.getElementById("senha_automatica").setAttribute("value", localStorage.getItem('senha'));
+	</script>
