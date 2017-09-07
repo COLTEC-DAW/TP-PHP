b8890b49ad1a052b828f64b475688d5a56421737
Lucas <lucaspaulom@hotmail.com>
2017-09-06 21:05:35 -0300

    Add files via upload

--- help.ME/usuario/conf_cadastro.php
+++ help.ME/usuario/conf_cadastro.php
@@ -60,39 +60,40 @@
         }
 
         if($existe == 0){
-            $dados = file_get_contents('users.json');
-            $json = json_decode($dados);
-
-
-            $json[] = array('login'=>$login, 'senha'=>$senha, 'nome'=>$nome, 'email'=>$email, 'carteira'=>0);
-
-
-            $dados_json = json_encode($json, JSON_PRETTY_PRINT);
-            $arquivo = fopen("users.json", "w");
-            fwrite($arquivo, $dados_json);
-            fclose($arquivo);
-            setcookie("checa_cadastro",true); 
-            $_COOKIE['checa_cadastro'] = true;
-
-//Escreve no arquivo informando que o email ainda não foi verificado;
-
-            $dados = file_get_contents('../email/emails_verificados.json');
-            $json = json_decode($dados);
-
             $id_email_conf = mt_rand();
-
-
-            $json[] = array('login'=>$login, 'email'=>$email, 'id_email_conf'=>$id_email_conf, 'verificado'=>0);
-            $dados_json = json_encode($json, JSON_PRETTY_PRINT);
-            $arquivo = fopen("../email/emails_verificados.json", "w");
-            fwrite($arquivo, $dados_json);
-            fclose($arquivo);
-
             $retorno = sendEmail($email, $id_email_conf, $login);
-            echo "<br>".$retorno;
-            $redirect = "../index.php";
-            if($retorno == 'done')
+            if($retorno == 'done'){
+                $dados = file_get_contents('users.json');
+                $json = json_decode($dados);
+
+
+                $json[] = array('login'=>$login, 'senha'=>$senha, 'nome'=>$nome, 'email'=>$email, 'carteira'=>0);
+
+
+                $dados_json = json_encode($json, JSON_PRETTY_PRINT);
+                $arquivo = fopen("users.json", "w");
+                fwrite($arquivo, $dados_json);
+                fclose($arquivo);
+                setcookie("checa_cadastro",true); 
+                $_COOKIE['checa_cadastro'] = true;
+
+                //Escreve no arquivo informando que o email ainda não foi verificado;
+
+                $dados = file_get_contents('../email/emails_verificados.json');
+                $json = json_decode($dados);
+
+
+
+                $json[] = array('login'=>$login, 'email'=>$email, 'id_email_conf'=>$id_email_conf, 'verificado'=>0);
+                $dados_json = json_encode($json, JSON_PRETTY_PRINT);
+                $arquivo = fopen("../email/emails_verificados.json", "w");
+                fwrite($arquivo, $dados_json);
+                fclose($arquivo);
+
+                echo "<br>".$retorno;
+                $redirect = "../index.php";
                 header("location:$redirect");
+            }
         }
     }
     else
