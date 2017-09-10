d69869196bb6fe6580b0b87b43728c6be4f204cd
arthurbrito99 <arthur.brito1999@outlook.com>
2017-09-10 14:01:43 -0300

    Alterando mensagem de login automático.

--- help.ME/utils/login_automatico.php
+++ help.ME/utils/login_automatico.php
@@ -3,19 +3,15 @@
     $senha = "<script>document.writeln(localStorage.getItem('senha'));</script>";
 ?>
 
-    <div class="card">
 
 	    <form action="../usuario/conf_login.php" method="post" enctype="multipart/form-data">
 
 	        <input id="login_automatico" type="hidden" name="nome" value="">
 	        <input id="senha_automatica" type="hidden" name="senha" value="">
-	        <p class="center-align">Continuar como <?=$login?>?</p>
-	        <div class="center-align input-field">
-	            <input type="submit" name="Enviar" class="btn" value="Sim">
-	        </div>
+	        <b class="center-align">Continuar como <?=$login?>?</b><br>
+	    	<input type="submit" name="Enviar" class="btn btn-small" value="Sim">
 
 	    </form>
-	</div>
 
 	<script type="text/javascript">
 		document.getElementById("login_automatico").setAttribute("value", localStorage.getItem('login'));
